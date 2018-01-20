f7ccae6b04c86f8f75592dd9e5ad48c765187c8a
dillonchristensen <Dillongc21@gmail.com>
2018-01-20 15:57:15 -0700

    Initial week02 commit

--- web/index.php
+++ web/index.php
@@ -1,5 +1,17 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <link rel="stylesheet" href="css/simplegrid.css">
+    <title>Dillon Christensen- CS 313</title>
+</head>
+<body>
+    <div class="grid grid-pad">
+        <div id="header" class="col-1-1">
+            <h1>Dillon Christensen</h1>
+            <h3>CS 313: Web Engineering II -- Brother Burton</h3>
+        </div>
+    </div>
+</body>
+</html>
